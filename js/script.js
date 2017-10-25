cf2c34a7b2678df10a6ff939fbaee5cfd253ccde
ivanacensi <ivan.elimbi@acensi.fr>
2017-10-25 12:30:35 +0200

    First test of the jquery getjson method

--- js/script.js
+++ js/script.js
@@ -14,10 +14,27 @@ function loadData() {
     // load streetview
     $body.append('<img class="bgimg" src=http://maps.googleapis.com/maps/api/streetview?size=600x300&location=79%20Rue%20Quincampoix,Paris">');
     // YOUR CODE GOES HERE!
-   
-
+    var city = "paris"; 
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+      'api-key': "843ec920599e452fa83ab934c7ebb0f6", 
+      'q': city
+    });
+    $.getjson({
+      url: url,
+      method: 'GET',
+      data: data,
+      success: function(data){
+          console.log(data); 
+      }
+    }).done(function(result) {
+      console.log(result);
+    }).fail(function(err) {
+      throw err;
+    });
+    
     return false;
-};
+}
 
 $('#form-container').submit(loadData);
 
